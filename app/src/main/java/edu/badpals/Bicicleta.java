1cddb138fe8c9b214cc21d804371387743ffa98d
davidbua <bua.teijeiro.david@gmail.com>
2024-02-22 12:28:24 +0100

    feat(Bicileta.java): Añadir constructor de la clase Bicicleta

---
+++ app/src/main/java/edu/badpals/Bicicleta.java
@@ -0,0 +1,14 @@
+package edu.badpals;
+
+public class Bicicleta {
+    final int id;
+
+    public Bicicleta(int id){
+        this.id = id;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+    
+}
