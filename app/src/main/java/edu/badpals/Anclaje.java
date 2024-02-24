e23a88a399c1837250248d5753c2590535733e01
BuaTeijeiro <bua.teijeiro.david@gmail.com>
2024-02-24 18:54:08 +0100

    feat(Anclaje.java): Añadir clase Anclaje con su constructor.

---
+++ app/src/main/java/edu/badpals/Anclaje.java
@@ -0,0 +1,23 @@
+package edu.badpals;
+
+public class Anclaje {
+    private boolean ocupado;
+    private Bicicleta bici;
+
+    public Anclaje(){
+        this.ocupado = false;
+    }
+
+    public boolean isOcupado(){
+        return this.ocupado;
+    }
+
+    private void setOcupado(boolean updateOcupado){
+        this.ocupado = updateOcupado;
+    }
+
+    public void anclarBici(Bicicleta newBici){
+        this.bici = newBici;
+        setOcupado(true);
+    }
+}
--- app/src/test/java/edu/badpals/AnclajeTest.java
+++ app/src/test/java/edu/badpals/AnclajeTest.java
@@ -8,26 +8,35 @@ import org.junit.Test;
 
 
 
-public class BicicletaTest {
+public class AnclajeTest {
 
-    Bicicleta bici = null;
+    Bicicleta bici ;
+    Anclaje anclaje;
     
     @Before 
     public void setup() {
         this.bici = new Bicicleta(999);
+        this.anclaje = new Anclaje();
     }
 
     @Test
-    public void constructorBiciTest() {
-        assertEquals(999, bici.getId());
+    public void constructorAnclajeTest() {
+        assertEquals(false, anclaje.isOcupado());
     }
 
+    @Test
+    public void anclarBiciTest() {
+        anclaje.anclarBici(bici);
+        assertEquals(true, anclaje.isOcupado());
+    }
+
+    /*
     @Test
     public void toStringTest() {
         assertEquals("999", bici.toString());
     }
 
-    /*
+    
     @Test
     public void implementationMovilTest() {
         Movil movil = bici;
